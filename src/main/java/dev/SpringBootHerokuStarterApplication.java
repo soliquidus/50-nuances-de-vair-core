df9347723658e72fb95056a88382120cb8ce4b3e
soliquidus <tibo.pfeifer@gmail.com>
2022-01-17 14:50:48 +0100

    Initialisation de la branche develop

--- src/main/java/dev/SpringBootHerokuStarterApplication.java
+++ src/main/java/dev/SpringBootHerokuStarterApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootHerokuStarterApplication {
 
+//    Initialisation branche develop
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHerokuStarterApplication.class, args);
     }
