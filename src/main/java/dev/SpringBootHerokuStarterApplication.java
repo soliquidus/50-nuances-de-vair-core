8fe6ad3900d0aced9b5acf5e684af827417d5e37
Tibo Pfeifer <tibo.pfeifer@gmail.com>
2022-01-17 15:43:16 +0100

    Initialisation de la branche develop avec flow

--- src/main/java/dev/SpringBootHerokuStarterApplication.java
+++ src/main/java/dev/SpringBootHerokuStarterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootHerokuStarterApplication {
 
-//    Initialisation branche develop
+//    Initialisation branche develop avec flow
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHerokuStarterApplication.class, args);
