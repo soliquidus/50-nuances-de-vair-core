055760f6225c417aeba929f8ac191817caac7934
Romain <lemaire.romain37@gmail.com>
2022-01-18 10:21:39 +0100

    Add @Entity into Admin

--- src/main/java/dev/entity/Admin.java
+++ src/main/java/dev/entity/Admin.java
@@ -1,6 +1,11 @@
 package dev.entity;
 
-public class Admin extends User{
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "elbat")
+public class Admin extends User {
 
 	public Admin(String userName, String firstName, String lastName, String email, String password,
 			Boolean activeAccount) {
