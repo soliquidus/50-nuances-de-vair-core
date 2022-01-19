0522242f8cb59a753dc0c6b7eee80de1197b0936
yvan <https://github.com/Pallies>
2022-01-19 14:17:09 +0100

    WeatherLabel error function byIcon

--- src/main/java/dev/enums/WeatherLabel.java
+++ src/main/java/dev/enums/WeatherLabel.java
@@ -1,13 +1,17 @@
 package dev.enums;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
 
 public enum WeatherLabel {
     CLEAR_SKY("clear sky", "01d.png"),
     FEW_CLOUDS("few clouds", "02d.png"),
     SCATTERED_CLOUDS("scattered clouds", "03d.png"),
-    OVERCAST_CLOUDS("overcast clouds","04d.png"),
+    OVERCAST_CLOUDS("overcast clouds", "04d.png"),
     SHOWER_RAIN("shower rain", "09d.png"),
     RAIN("rain", "10d.png"),
     THUNDERSTORM("thunderstorm", "11d.png"),
@@ -21,12 +25,12 @@ public enum WeatherLabel {
         this.label = label;
         this.icon = icon;
     }
-    public static WeatherLabel findWeatherLabelByIcon(String icon){
-        return Arrays.stream(WeatherLabel.values())
-                .filter(w->w.getIcon().equals(icon))
-                .collect(Collectors.toList()).get(0);
-    }
-    public String getIcon() {
-        return icon;
+
+    public static WeatherLabel findWeatherLabelByIcon(String icon) {
+        for (WeatherLabel c : WeatherLabel.values()) {
+            if (c.icon.equals(icon))
+                return c;
+        }
+        return CLEAR_SKY;
     }
 }
