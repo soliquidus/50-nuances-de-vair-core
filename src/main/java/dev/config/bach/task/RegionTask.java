539f49eb39da5251c703eb304f6d029a144fcde9
yvan <https://github.com/Pallies>
2022-01-19 21:16:52 +0100

    add region task

---
+++ src/main/java/dev/config/bach/task/RegionTask.java
@@ -0,0 +1,29 @@
+package dev.config.bach.task;
+
+import dev.dto.RegionDto;
+import dev.service.RegionService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
+@PropertySource("classpath:application-api.properties")
+public class RegionTask {
+    @Value("${request.region-geo-gouv}")
+    private String urlRegion;
+
+    private final RegionService regionService;
+
+    public RegionTask(RegionService regionService) {
+        this.regionService = regionService;
+    }
+    /* /!\ fisrt request before DepartmentTask and CityTask*/
+    public void run(RestTemplate restTemplate ){
+        RegionDto[] regionsDto = restTemplate.getForObject(urlRegion, RegionDto[].class);
+        assert regionsDto != null;
+        this.regionService.createRegions(Arrays.asList(regionsDto));
+    }
+}
