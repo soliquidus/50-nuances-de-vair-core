cd8f7eaafd841f5608c919db608ba47f84858f44
yvan <https://github.com/Pallies>
2022-01-19 09:43:36 +0100

    create shcedule for webClient

---
+++ src/main/java/dev/config/ApiScheduledCall.java
@@ -0,0 +1,36 @@
+package dev.config;
+
+
+import dev.entity.Pollution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+
+@Configuration
+@EnableScheduling
+public class ApiScheduledCall {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(ApiScheduledCall.class);
+
+
+    private final ApplicationContext context;
+
+    public ApiScheduledCall(ApplicationContext context) {
+        this.context = context;
+    }
+
+    //    @Scheduled(cron = "0 0 6 * * ?")
+    @Scheduled(fixedDelay = 1000)
+    public void scheduleFixedDelayTask() {
+        Pollution pollution = context.getBean("runPollutionTask", Pollution.class);
+
+        LOGGER.info("{} polution entity to save {} ", LocalDateTime.now(), pollution);
+    }
+
+}
