fe698107d2e33f3c2b5d62bc32eda6c484e3c0c1
yvan <https://github.com/Pallies>
2022-01-19 09:44:51 +0100

    webClient

---
+++ src/main/java/dev/config/RestTemplateConfig.java
@@ -0,0 +1,22 @@
+package dev.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    public RestTemplateConfig(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
+    @Bean
+    /* build web client */
+    public RestTemplate buildTemplate() {
+        return restTemplateBuilder.build();
+    }
+}
