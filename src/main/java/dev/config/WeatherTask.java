0da817fd7ddcc24e927a0f38fa224e88b7759060
yvan <https://github.com/Pallies>
2022-01-19 14:16:28 +0100

    error configuration context

--- src/main/java/dev/config/WeatherTask.java
+++ src/main/java/dev/config/WeatherTask.java
@@ -6,13 +6,15 @@ import dev.entity.Weather;
 import dev.repository.WeatherRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 @PropertySource("classpath:application-api.properties")
 public class WeatherTask {
-    @Value("${key.api-open-weather}")
+   @Value("${key.api-open-weather}")
     private String keyApiWeather;
     @Value("${request.weather-open-weather}")
     private String urlWeather;
@@ -24,6 +26,7 @@ public class WeatherTask {
     public WeatherTask(WeatherRepository weatherRepository) {
         this.weatherRepository = weatherRepository;
     }
+
     @Bean
     @Scope("prototype")
     public Weather runWeatherTask(RestTemplate restTemplate){
@@ -32,10 +35,11 @@ public class WeatherTask {
         city.setZipCode(85600);
         /* request api open-weather */
         WeatherDto weatherDto = restTemplate.getForObject(
-                String.format(urlWeather,city.getName(),state,city.getZipCode())
+                String.format(urlWeather,
+                        city.getName(),state,city.getZipCode(),keyApiWeather)
         ,WeatherDto.class);
         /* format response */
         Weather weather = new Weather(weatherDto);
-        return weatherRepository.save(null);
+        return weatherRepository.save(weather);
     }
 }
