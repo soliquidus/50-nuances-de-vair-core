7614892d9b45b04a28b4d6f620c5a4301af2e517
yvan <https://github.com/Pallies>
2022-01-19 12:48:37 +0100

    add findWeatherLabelByIcon

--- src/main/java/dev/enums/WeatherLabel.java
+++ src/main/java/dev/enums/WeatherLabel.java
@@ -1,10 +1,13 @@
 package dev.enums;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum WeatherLabel {
     CLEAR_SKY("clear sky", "01d.png"),
     FEW_CLOUDS("few clouds", "02d.png"),
     SCATTERED_CLOUDS("scattered clouds", "03d.png"),
-    BROKEN_CLOUDS("broken clouds", "04d.png"),
+    OVERCAST_CLOUDS("overcast clouds","04d.png"),
     SHOWER_RAIN("shower rain", "09d.png"),
     RAIN("rain", "10d.png"),
     THUNDERSTORM("thunderstorm", "11d.png"),
@@ -17,5 +20,13 @@ public enum WeatherLabel {
     WeatherLabel(String label, String icon) {
         this.label = label;
         this.icon = icon;
+    }
+    public static WeatherLabel findWeatherLabelByIcon(String icon){
+        return Arrays.stream(WeatherLabel.values())
+                .filter(w->w.getIcon().equals(icon))
+                .collect(Collectors.toList()).get(0);
+    }
+    public String getIcon() {
+        return icon;
     }
 }
