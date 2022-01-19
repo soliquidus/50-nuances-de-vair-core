dda6a67f859efc08ee4b9d176e2e6649466bf0db
yvan <https://github.com/Pallies>
2022-01-19 09:40:10 +0100

    return QualityAir by index

--- src/main/java/dev/enums/QualityAir.java
+++ src/main/java/dev/enums/QualityAir.java
@@ -1,9 +1,15 @@
 package dev.enums;
 
+import java.util.List;
+
 public enum QualityAir {
     GOOD,
     FAIR,
     MODERATE,
     POOR,
     VERY_POOR;
+
+    public static QualityAir byIndex(int i) {
+        return List.of(QualityAir.values()).get(i);
+    }
 }
