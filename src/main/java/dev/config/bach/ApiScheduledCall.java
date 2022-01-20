05a09877ca555115ea13c7d1a27b97be006d0764
yvan <https://github.com/Pallies>
2022-01-20 01:10:29 +0100

    launcher comments

--- src/main/java/dev/config/bach/ApiScheduledCall.java
+++ src/main/java/dev/config/bach/ApiScheduledCall.java
@@ -3,13 +3,16 @@ package dev.config.bach;
 
 import dev.config.bach.controller.TaskController;
 import dev.entity.City;
+import dev.entity.Weather;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,35 +23,31 @@ public class ApiScheduledCall {
     private final Logger LOGGER = LoggerFactory.getLogger(ApiScheduledCall.class);
     private final TaskController taskController;
 
-
     public ApiScheduledCall(TaskController taskController) {
         this.taskController = taskController;
     }
 
     @PostConstruct
     public void cityInitializeWithRelation() {
-       taskController.regionTaskController();
+//        taskController.regionTaskController();
+//        taskController.departmentTaskController();
+//        taskController.cityTaskController();
     }
 
     //    @Scheduled(cron = "0 0 6 * * ?")
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(fixedDelay = 10000000)
     public void scheduleFixedDelayTask() {
-        // boucle sur les départements
-//        appel de la liste de la ville du département
-
-
-//            Pollution pollution = taskController.pollutionTaskController(city);
-//            Weather weather = taskController.
-
-//        boucle sur les villes
-        // requêtes pollution
-//        Pollution pollution = context.getBean("runPollutionTask", Pollution.class);
-        // requêtes weather
-//        Weather weather = context.getBean("runWeatherTask",Weather.class);
-//        enregistrement de la ville avec pollution / weather / census
-
-        LOGGER.info("MESSAGE scheduleFixedDelayTask ");
-
+//        List<City> citiesError = new ArrayList<>();
+//        taskController.getAllCities().forEach(city->{
+//            try{
+//                Weather weather = taskController.weatherTaskControler(city);
+//                city.setWeather(weather);
+//                taskController.citySaveByTaskController(city);
+//            }catch (HttpClientErrorException e){
+//                citiesError.add(city);
+//            }
+//        });
+//        citiesError.forEach(c->LOGGER.info("error database for the city : {}",c));
     }
 
 }
