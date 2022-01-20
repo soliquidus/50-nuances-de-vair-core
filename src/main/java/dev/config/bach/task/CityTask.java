aea41e9efc26312816d157dde655dac5714e8f67
yvan <https://github.com/Pallies>
2022-01-20 01:09:01 +0100

    task for save the cities in database

--- src/main/java/dev/config/bach/task/CityTask.java
+++ src/main/java/dev/config/bach/task/CityTask.java
@@ -7,7 +7,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @PropertySource("classpath:application-api.properties")
@@ -15,16 +18,51 @@ public class CityTask {
 
     @Value("${request.cities-geo-gouv}")
     private String urlCities;
+    @Value(("${state.department-geo.gouv}"))
+    private List<String> specialDeptpartmentCode;
+
     private final CityService cityService;
 
     public CityTask(CityService cityService) {
         this.cityService = cityService;
     }
     /* /!\ Task programming after DepartmentTask */
-    public void run(RestTemplate restTemplate, Integer codeDept){
-        String url = String.format(urlCities,codeDept);
-        CityDto[] cityDto = restTemplate.getForObject(url, CityDto[].class);
-        assert cityDto != null;
-        this.cityService.createCities(List.of(cityDto));
+    public void run(RestTemplate restTemplate) {
+        List<String> codeDept = createListDepartment();
+        codeDept.addAll(specialDeptpartmentCode);
+        codeDept.forEach(code -> {
+            new Timer().schedule(
+                    new CityWebClient(restTemplate, cityService, code)
+                    , 10000, 10000);
+        });
+
+    }
+
+    private List<String> createListDepartment() {
+        AtomicInteger index = new AtomicInteger(1);
+        return Stream.of(new Integer[95])
+                .map(c -> index.getAndIncrement())
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    class CityWebClient extends TimerTask {
+        private final RestTemplate restTemplate;
+        private final CityService cityService;
+        private final String codeDept;
+
+        public CityWebClient(RestTemplate restTemplate, CityService cityService, String codeDept) {
+            this.restTemplate = restTemplate;
+            this.cityService = cityService;
+            this.codeDept = codeDept;
+        }
+
+        @Override
+        public void run() {
+            String url = String.format(urlCities, codeDept);
+            CityDto[] cityDto = restTemplate.getForObject(url, CityDto[].class);
+            assert cityDto != null;
+            this.cityService.createCities(List.of(cityDto));
+        }
     }
 }
