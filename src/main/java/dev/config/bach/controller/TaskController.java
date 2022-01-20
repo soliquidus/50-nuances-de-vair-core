85b22e751ac5b0efd66df2a2fd35592f9ed36888
yvan <https://github.com/Pallies>
2022-01-20 01:07:52 +0100

    controller task

--- src/main/java/dev/config/bach/controller/TaskController.java
+++ src/main/java/dev/config/bach/controller/TaskController.java
@@ -1,14 +1,13 @@
 package dev.config.bach.controller;
 
-import dev.config.bach.task.CensusTask;
-import dev.config.bach.task.PollutionTask;
+
+import dev.config.bach.task.CityTask;
+import dev.config.bach.task.DepartmentTask;
 import dev.config.bach.task.RegionTask;
 import dev.config.bach.task.WeatherTask;
 import dev.entity.City;
-import dev.entity.Pollution;
 import dev.entity.Weather;
 import dev.repository.CityRepository;
-import dev.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
@@ -18,56 +17,66 @@ import java.util.List;
 @Controller
 public class TaskController {
 
-    private CensusTask censusTask;
+    //    private CensusTask censusTask;
+//    private PollutionTask pollutionTask;
     private WeatherTask weatherTask;
-    private PollutionTask pollutionTask;
     private RestTemplate restTemplate;
     private RegionTask regionTask;
+    private DepartmentTask departmentTask;
+    private CityTask cityTask;
     private CityRepository cityRepository;
 
-//    public TaskController(CensusTask censusTask, WeatherTask weatherTask, PollutionTask pollutionTask, RestTemplate restTemplate,CityService cityService,CityRepository cityRepository) {
-//        this.censusTask = censusTask;
-//        this.weatherTask = weatherTask;
-//        this.pollutionTask = pollutionTask;
-//        this.restTemplate = restTemplate;
-//        this.cityRepository=cityRepository;
-//    }
-
-    public Pollution pollutionTaskController(City city) {
-        return this.pollutionTask.run(restTemplate, city);
-    }
 
     public Weather weatherTaskControler(City city) {
         return this.weatherTask.run(restTemplate, city);
     }
 
-    public void cityTaskController() {
-
-    }
-    public void regionTaskController(){
+    public void regionTaskController() {
         regionTask.run(restTemplate);
     }
 
+    public void departmentTaskController() {
+        departmentTask.run(restTemplate);
+    }
+
+    public void cityTaskController() {
+        cityTask.run(restTemplate);
+    }
+    public void citySaveByTaskController(City city){
+        cityRepository.save(city);
+    }
     public List<City> getAllCities() {
         return cityRepository.findAll();
     }
 
-    @Autowired
-    public void setCensusTask(CensusTask censusTask) {
-        this.censusTask = censusTask;
-    }
+    //    @Autowired
+//    public void setCensusTask(CensusTask censusTask) {
+//        this.censusTask = censusTask;
+//    }
+//    @Autowired
+//    public void setPollutionTask(PollutionTask pollutionTask) {
+//        this.pollutionTask = pollutionTask;
+//    }
     @Autowired
     public void setWeatherTask(WeatherTask weatherTask) {
         this.weatherTask = weatherTask;
     }
-    @Autowired
-    public void setPollutionTask(PollutionTask pollutionTask) {
-        this.pollutionTask = pollutionTask;
-    }
+
     @Autowired
     public void setRegionTask(RegionTask regionTask) {
         this.regionTask = regionTask;
     }
+
+    @Autowired
+    public void setDepartmentTask(DepartmentTask departmentTask) {
+        this.departmentTask = departmentTask;
+    }
+
+    @Autowired
+    public void setCityTask(CityTask cityTask) {
+        this.cityTask = cityTask;
+    }
+
     @Autowired
     public void setRestTemplate(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
