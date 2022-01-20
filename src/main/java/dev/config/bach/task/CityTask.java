6740d8d36c800949fe76e40e6ee6de5842914b1c
yvan <https://github.com/Pallies>
2022-01-20 15:45:56 +0100

    rectif CityTask

--- src/main/java/dev/config/bach/task/CityTask.java
+++ src/main/java/dev/config/bach/task/CityTask.java
@@ -2,11 +2,15 @@ package dev.config.bach.task;
 
 import dev.dto.CityDto;
 import dev.service.CityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.transaction.Transactional;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -15,10 +19,10 @@ import java.util.stream.Stream;
 @Service
 @PropertySource("classpath:application-api.properties")
 public class CityTask {
-
+private Logger LOGGER = LoggerFactory.getLogger(CityTask.class);
     @Value("${request.cities-geo-gouv}")
     private String urlCities;
-    @Value(("${state.department-geo.gouv}"))
+    @Value("${state.department-geo.gouv}")
     private List<String> specialDeptpartmentCode;
 
     private final CityService cityService;
@@ -26,46 +30,34 @@ public class CityTask {
     public CityTask(CityService cityService) {
         this.cityService = cityService;
     }
+
     /* /!\ Task programming after DepartmentTask */
+    @Transactional
     public void run(RestTemplate restTemplate) {
-        /* codeDept is a list of departments */
         List<String> codeDept = createListDepartment();
         codeDept.addAll(specialDeptpartmentCode);
-
+        long start = System.currentTimeMillis();
         codeDept.forEach(code -> {
-            /* timer to regulate the flow */
-            new Timer().schedule(
-                    new CityWebClient(restTemplate, cityService, code)
-                    , 10000, 10000);
+            String url = String.format(urlCities, code);
+            try{
+                CityDto[] cityDto = restTemplate.getForObject(url, CityDto[].class);
+                assert cityDto != null;
+                this.cityService.createCities(List.of(cityDto));
+            }catch (HttpClientErrorException e){
+                LOGGER.info(url);
+            }
         });
-
+        long end = System.currentTimeMillis();
+        LOGGER.info(" Temps exécution : {}",(end-start)/1000);
     }
-    /* create the list of departments */
+
     private List<String> createListDepartment() {
         AtomicInteger index = new AtomicInteger(1);
         return Stream.of(new Integer[95])
                 .map(c -> index.getAndIncrement())
+                .filter(i->i!=20)
                 .map(String::valueOf)
+                .map(str->str.length()==1?"0"+str:str)
                 .collect(Collectors.toList());
-    }
-    /* task managed by the timer */
-    class CityWebClient extends TimerTask {
-        private final RestTemplate restTemplate;
-        private final CityService cityService;
-        private final String codeDept;
-
-        public CityWebClient(RestTemplate restTemplate, CityService cityService, String codeDept) {
-            this.restTemplate = restTemplate;
-            this.cityService = cityService;
-            this.codeDept = codeDept;
-        }
-
-        @Override
-        public void run() {
-            String url = String.format(urlCities, codeDept);
-            CityDto[] cityDto = restTemplate.getForObject(url, CityDto[].class);
-            assert cityDto != null;
-            this.cityService.createCities(List.of(cityDto));
-        }
     }
 }
