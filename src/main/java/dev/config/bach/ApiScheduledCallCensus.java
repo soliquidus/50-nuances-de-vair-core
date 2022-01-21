f0b7d1dfa019e76ebda34c5cf27b3e5cf307cb47
yvan <https://github.com/Pallies>
2022-01-21 13:15:46 +0100

    add task update data cencus city

--- src/main/java/dev/config/bach/ApiScheduledCallCensus.java
+++ src/main/java/dev/config/bach/ApiScheduledCallCensus.java
@@ -1,5 +1,6 @@
 package dev.config.bach;
 
+import dev.service.CityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,16 @@ import org.springframework.scheduling.annotation.Scheduled;
 @EnableScheduling
 public class ApiScheduledCallCensus {
     private final Logger LOGGER = LoggerFactory.getLogger(ApiScheduledCallCensus.class);
+    private final CityService cityService;
+
+    public ApiScheduledCallCensus(CityService cityService) {
+        this.cityService = cityService;
+    }
+
     /* population data update every new year */
     @Scheduled(cron = "0 0 0 1 1 ?")
     public void scheduleFixedDelayTask() {
+        cityService.getGeoData();
         LOGGER.info("Maj population");
     }
 }
